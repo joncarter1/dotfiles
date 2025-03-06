f13cd140356036257617eb82ce78dcda514ac8e3
Jonathan Carter <jonathan.carter@magd.ox.ac.uk>
2025-03-06 13:29:43 +0000

    Add fzf to install
    Signed-off-by: Jonathan Carter <jonathan.carter@magd.ox.ac.uk>

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install dotfiles
 ./scripts/dotfiles.sh backup
 ./scripts/dotfiles.sh prune
 stow --verbose --target=${HOME} --dotfiles --no-folding --restow dotfiles/
+
+# Install fzf
+./scripts/install/fzf.sh
