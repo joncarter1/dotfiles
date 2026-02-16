56e608be3b15d8e5544a489af79bcefebacaccca
Jonathan Carter <42900403+joncarter1@users.noreply.github.com>
2026-02-16 11:47:46 +0000

    updates

--- scripts/install/fzf.sh
+++ scripts/install/fzf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --no-bash
 
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
