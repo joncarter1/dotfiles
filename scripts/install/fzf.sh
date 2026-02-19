85170ec608badbf1ad6cc5d377b02edf813cd47a
Jonathan Carter <42900403+joncarter1@users.noreply.github.com>
2026-02-19 08:27:05 +0000

    fix fzf install on instances
    Signed-off-by: Jonathan Carter <jonathan.carter.1@outlook.com>

--- scripts/install/fzf.sh
+++ scripts/install/fzf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --no-bash
+~/.fzf/install --all --no-update-rc
 
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
