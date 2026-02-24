14cbc245e5091fe801ffd6fd6b2e33cc9ae120d7
Jonathan Carter <42900403+joncarter1@users.noreply.github.com>
2026-02-24 15:48:20 +0000

    handle fzf already cloned
    Signed-off-by: Jonathan Carter <42900403+joncarter1@users.noreply.github.com>

--- scripts/install/fzf.sh
+++ scripts/install/fzf.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+if [ -d ~/.fzf ]; then
+  git -C ~/.fzf pull
+else
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+fi
 ~/.fzf/install --all --no-update-rc
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
